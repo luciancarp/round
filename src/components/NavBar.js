e769efe087fd8d3967160f9dac7df60094be97ae
Lucian Carp <lc16929@bristol.ac.uk>
2019-02-10 20:14:20 +0000

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { palette, spaces } from '../styles/styles'
+import styled from 'styled-components'
+
+const StyledNavBar = styled.div`
+  display: flex;
+  background-color: ${palette.backgroundColor};
+  height: 75px;
+  padding: ${spaces.medium}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 5px solid ${palette.accentColor};
+`
+const StyledPar = styled.h1`
+  color: ${palette.text};
+`
+const StyledLogo = styled.img`
+  height: 50px;
+  width: 50px;
+`
+const BorderNavBar = styled.div`
+  height: 10px;
+  background-color: ${palette.primaryColor}
+`
+
+class NavBar extends Component {
+  render () {
+    return (
+      <div>
+        <StyledNavBar>
+          <StyledLogo src={require('../assets/images/logo_small.png')} />
+          <StyledPar>Hello there</StyledPar>
+        </StyledNavBar>
+        <BorderNavBar />
+      </div>
+    )
+  }
+}
+
+export default NavBar
---
+++ src/styles/styles.js
@@ -0,0 +1,22 @@
+export const fonts = {
+  regular: 'Open Sans',
+  bold: 'Open Sans Bold'
+}
+
+export const fontSizes = {
+  title: 32,
+  regular: 14
+}
+
+export const spaces = {
+  narrow: 10,
+  medium: 14,
+  wide: 22
+}
+
+export const palette = {
+  primaryColor: '#ff4500',
+  accentColor: '#00eaff',
+  backgroundColor: '#1f1f1f',
+  text: '#d3d3d3'
+}
